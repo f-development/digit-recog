de5a2e74a500b550fced12bffde731465bfa3fc9
KEEWOONG NAM <keewoong.nam@flicspy.com>
2024-06-04 10:52:08 +0900

    Maintenance

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,17 @@
 #include "handwritten_digit_analyzer.h"
 
 int main() {
-	double learningRate = 20;
+	double learningRate = 10;
 
-	digit_recognizer::handwritten_digits_analyzer analyzer({ 30 }, learningRate);
+	digit_recognizer::handwritten_digits_analyzer analyzer({ 100, 30 }, learningRate);
 
-	size_t numEpochs = 50;
+	size_t numEpochs = 30;
 
 	analyzer.trainWithBinaryFile(numEpochs,
-    "../../data/mnist/train-images.idx3-ubyte",
-		"../../data/mnist/train-labels.idx1-ubyte",
-		"../../data/mnist/t10k-images.idx3-ubyte",
-		"../../data/mnist/t10k-labels.idx1-ubyte");
+    "./data/mnist/train-images.idx3-ubyte",
+		"./data/mnist/train-labels.idx1-ubyte",
+		"./data/mnist/t10k-images.idx3-ubyte",
+		"./data/mnist/t10k-labels.idx1-ubyte");
 
 	//analyzer.trainWithTextFile(numEpochs, "../../../data/mnist/mnist_train.csv",
 		//"../../../data/mnist/mnist_test.csv");
